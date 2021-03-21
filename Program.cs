0e260b5beb92767340dd0db37504a8b0095ee495
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-21 13:48:01 -0300

    Exercicio Milhas para Km

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_Milhas_Para_Km
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                double milhas, km = 1.609, resultado; 
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+               Console.Write("-----Milhas para Km-----");
+               Console.WriteLine();
+               Console.Write("Conversão de Milhas para KM");
+               Console.WriteLine();
+               Console.Write("Digite um Numero:");
+               Console.ResetColor();
+
+                 milhas = Convert.ToDouble(Console.ReadLine());
+                
+                
+                     resultado = milhas*km;
+
+                     Console.ForegroundColor = ConsoleColor.Green;
+                     Console.WriteLine($"{resultado:N3}");
+                     Console.ResetColor();
+            
+        }
+    }
+}
